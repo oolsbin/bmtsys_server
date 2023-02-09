c03be2520f818c27c7ef07faccbd956983bc331a
oolsbin <ooisv@naver.com>
2023-02-10 08:26:19 +0900

    02-10(company)

--- Hello/src/main/java/com/example/demo/HelloApplication.java
+++ Hello/src/main/java/com/example/demo/HelloApplication.java
@@ -1,15 +1,11 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude= {DataSourceAutoConfiguration.class})
-//데이터베이스 설정 안함 --> 뒤에서 데이터베이스 설정 작업 별개로 진행
 public class HelloApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
